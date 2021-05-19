c338ad5a70a53ae1d6e7f3a424916a4445c460fd
Rocc  Colonna <colonna.rocco2000@gmail.com>
2021-05-19 15:12:54 +0200

    fine menu

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css"> 
+</head>
+<body>
+    
+    <!-- INIZIO HEADER -->
+    <header>
+        <div class="logo">
+            <img src="img/Google-Logo.png" alt="">
+            <h2>Privacy & Termini</h2>
+        </div>   
+        <nav class="menu">
+            <ul>
+                <li>
+                    Introduzione
+                </li>
+                <li>
+                    Nome sulla privacy
+                </li>
+                <li>
+                    Termini di servizio
+                </li>
+                <li>
+                    Tecnologie
+                </li>
+                <li>
+                    Domande Frequenti
+                </li>
+            </ul>
+        </nav> 
+    </header>
+    <!-- FINE HEADER -->
+
+    <!-- INIZIO MAIN -->
+    <main>
+        <div class="container">
+
+        </div>
+    </main>
+    <!-- FINE MAIN -->
+
+    <!-- INIZIO FOOTER -->
+    <!-- FINE FOOTER -->
+
+</body>
+</html>
